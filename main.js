585ff8ff0042749bc67e97837d3baa7a2db65c55
artuknees <artuknees@gmail.com>
2022-09-19 09:37:20 -0300

    GET

--- main.js
+++ main.js
@@ -1,22 +1,42 @@
 const API_KEY = 'live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR';
 
-const API_URL = 'https://api.thecatapi.com/v1/images/search?limit=3&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR';
+const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2';
+// const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR'
 
-const onClickNewImage = async () => {
-    const response = await fetch(API_URL);
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR';
+
+const spanError = document.getElementById('error');
+
+const loadRandomMichis = async () => {
+    const response = await fetch(API_URL_RANDOM);
     const data = await response.json();
+    // console.log('Random');
+    // console.log(data);
 
-    const img1 = document.getElementById('image1');
-    const img2 = document.getElementById('image2');
-    const img3 = document.getElementById('image3');
+    // if (response.status !== 200) {
+    //     spanError.innerHTML = "Hubo un error: " + response.status;
+    // } else {    
+        const img1 = document.getElementById('image1');
+        const img2 = document.getElementById('image2');
 
-
-    img1.src = data[0].url;
-    img2.src = data[1].url;
-    img3.src = data[2].url;
-
-
-    console.log(data);
+        img1.src = data[0].url;
+        img2.src = data[1].url;
+    // }
 };
 
-onClickNewImage(); // para que recargue al hacer F5
+const loadFavoritesMichis = async () => {
+    const response = await fetch(API_URL_FAVOURITES);
+
+    if (response.status !== 200) {
+        console.log('Error en favoritos');
+        console.log(response);
+        spanError.innerText = "Hubo un error: " + response.status;
+    } else {
+        const data = await response.json();
+        console.log('Favourites');
+        console.log(data);
+    };
+};
+
+loadRandomMichis(); // para que recargue al hacer F5
+loadFavoritesMichis(); // llamo tambien a los favoritos
