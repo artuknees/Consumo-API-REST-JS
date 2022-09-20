b04bb19c813d58968d42b1947269869233119482
artuknees <artuknees@gmail.com>
2022-09-20 09:12:04 -0300

    POST

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const API_KEY = 'live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR';
 
 // const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2';
-const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR'
+const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=40&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR'
 
 const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR';
 
@@ -28,9 +28,14 @@ const loadRandomMichis = async () => {
 
         const img1 = document.getElementById('image1');
         const img2 = document.getElementById('image2');
+        const btn1 = document.getElementById('btn1');
+        const btn2 = document.getElementById('btn2');
 
         img1.src = data[0].url;
         img2.src = data[1].url;
+
+        btn1.onclick = () => saveFavourite(data[0].id);
+        btn2.onclick = () => saveFavourite(data[1].id);
         }
 };
 
@@ -54,17 +59,30 @@ const loadFavoritesMichis = async () => {
     } else {
         const data = await res.json();
         console.log('Favourites: ',data);
+        data.forEach(item => {
+            const section = document.getElementById('favoritesMichis');
+            const article = document.createElement('article');
+            const img = document.createElement('img');
+            const btn = document.createElement('button');
+            const btnText = document.createTextNode('Sacar al michi de favoritos');
+            btn.appendChild(btnText);
+            img.src = item.image.url;
+            img.width = 150;
+            article.appendChild(img);
+            article.appendChild(btn);
+            section.appendChild(article);
+        })
     }
 };
 
-const saveFavourite = async () => {
+const saveFavourite = async (id) => {
     const res = await fetch(API_URL_FAVOURITES , {
         method:'POST', // por defecto es un GET
-        header: { // el tipo de respuesta que estoy esperando
+        headers: { // el tipo de respuesta que estoy esperando
             'Content-Type':'application/json',
         },
         body: JSON.stringify({
-            image_id: 12
+            image_id: id,
         }), 
     });
     const data = await res.json();
