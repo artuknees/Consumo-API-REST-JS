308bec33564e757e97edc9d86ae304e55ca5d98d
artuknees <artuknees@gmail.com>
2022-09-25 20:28:58 -0300

    headers

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
+// Para el curso
+// https://developers.thecatapi.com/
+
 const API_KEY = 'live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR';
 
 // const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2';
-const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=40&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR'
+const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites'
 
-const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2&api_key=live_RrvbyXnUF9NzymQk9bziXj81T9169lW9DTBTt6KIRGHfFIcG8SqKDusNLP1oTqGR';
+const API_URL_RANDOM = 'https://api.thecatapi.com/v1/images/search?limit=2';
+
+const API_URL_FAVOURITES_DELETE = (id) => `https://api.thecatapi.com/v1/favourites/${id}`;
 
 const spanError = document.getElementById('error');
 
-const load = async () => {
-    const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2';
-    const res = await fetch(API_URL_FAVOURITES);
-    console.log('Response: ',res);
-    const data = await res.json();
-    console.log(data);
-};
-
+// const load = async () => {
+//     const API_URL_FAVOURITES = 'https://api.thecatapi.com/v1/favourites?limit=2';
+//     const res = await fetch(API_URL_FAVOURITES);
+//     console.log('Response: ',res);
+//     const data = await res.json();
+//     console.log(data);
+// };
 
 const loadRandomMichis = async () => {
     const res = await fetch(API_URL_RANDOM);
@@ -50,7 +54,14 @@ const loadRandomMichis = async () => {
 
 const loadFavoritesMichis = async () => {
             
-    const res = await fetch(API_URL_FAVOURITES);
+    const res = await fetch(API_URL_FAVOURITES
+        ,{
+        method: 'GET',
+        headers: {
+            'X-API-KEY': API_KEY,
+        },
+    }
+    );
     console.log('response favourites: ',res);
     if(res.status!==200){
         try {
@@ -68,13 +79,21 @@ const loadFavoritesMichis = async () => {
     } else {
         const data = await res.json();
         console.log('Favourites: ',data);
+        const section = document.getElementById('favoritesMichis');
+        section.innerHTML = "";
+        const h2 = document.createElement('h2');
+        const h2Text = document.createTextNode('Gatitos favoritos');
+        h2.appendChild(h2Text);
+        section.appendChild(h2);
+
+
         data.forEach(item => {
-            const section = document.getElementById('favoritesMichis');
             const article = document.createElement('article');
             const img = document.createElement('img');
             const btn = document.createElement('button');
             const btnText = document.createTextNode('Sacar al michi de favoritos');
             btn.appendChild(btnText);
+            btn.onclick = () => deleteFavourite(item.id);
             img.src = item.image.url;
             img.width = 150;
             article.appendChild(img);
@@ -89,15 +108,64 @@ const saveFavourite = async (id) => {
         method:'POST', // por defecto es un GET
         headers: { // el tipo de respuesta que estoy esperando
             'Content-Type':'application/json',
+            // 'Content-Type':'text/plain', // con esto falla porque no lo soporta
+            'X-API-KEY': API_KEY,
+            
         },
-        body: JSON.stringify({
+        body: 
+        JSON.stringify({
             image_id: id,
         }), 
     });
     const data = await res.json();
     console.log('save: ',res);
+
+    if(res.status!==200){
+        try {
+            console.log('voy a internar en save favs');
+            const data = await res.json();
+            console.log('falle');
+            console.log(data);
+        } catch(err) {
+            console.log('me vine al error en favs save');
+            console.error(err); // to console it as an error
+            console.log('Error at favourites: ',err); // console it as string
+            spanError.innerText = "Hubo un error: " + res.status + ' - ' + err;
+        }
+    } else {
+        console.log('Gato guardado en favoritos');
+        loadFavoritesMichis();
+    };
 };
 
+const deleteFavourite = async (id) => {
+    const res = await fetch(API_URL_FAVOURITES_DELETE(id) , {
+        method:'DELETE', // por defecto es un GET
+        headers: { // el tipo de respuesta que estoy esperando
+            'X-API-KEY': API_KEY,
+        },
+
+    });
+    const data = await res.json();
+    console.log('delete: ',res);
+
+    if(res.status!==200){
+        try {
+            console.log('voy a internar en delete favos');
+            const data = await res.json();
+            console.log('falle');
+            console.log(data);
+        } catch(err) {
+            console.log('me vine al error en favs delete');
+            console.error(err); // to console it as an error
+            console.log('Error at favourites: ',err); // console it as string
+            spanError.innerText = "Hubo un error: " + res.status + ' - ' + err;
+        }
+    } else {
+        console.log('Gato eliminado de favoritos');
+        loadFavoritesMichis();
+    };
+};
 
 loadRandomMichis(); // para que recargue al hacer F5
 loadFavoritesMichis(); // llamo tambien a los favoritos
